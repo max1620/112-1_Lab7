0f686b0087c9668f5eca613fc408b4270cee157e
max1620 <x35713712@gmail.com>
2023-11-17 14:22:06 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "112-1_lab7"
+include(":app")
+ 
